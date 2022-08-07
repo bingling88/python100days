35fa37a73b06e4fa942e39b391fd525901cfa2c2
bingling88 <chenwu0505@gmail.com>
2022-08-06 23:08:04 -0700

    initialize

---
+++ main.py
@@ -0,0 +1,40 @@
+MENU = {
+    "espresso": {
+        "ingredients": {
+            "water": 50,
+            "coffee": 18,
+        },
+        "cost": 1.5,
+    },
+    "latte": {
+        "ingredients": {
+            "water": 200,
+            "milk": 150,
+            "coffee": 24,
+        },
+        "cost": 2.5,
+    },
+    "cappuccino": {
+        "ingredients": {
+            "water": 250,
+            "milk": 100,
+            "coffee": 24,
+        },
+        "cost": 3.0,
+    }
+}
+
+resources = {
+    "water": 300,
+    "milk": 200,
+    "coffee": 100,
+}
+
+
+def check_user():
+    user_choice=input("What would you like?(espresso/latte/cappuccino):").lower()
+    return user_choice
+
+choice=check_user()
+while choice=="report":
+    print(f"Water: {resources["water"]}ml")
